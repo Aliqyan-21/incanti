141f3863886256b44fdfed5758ce11fef2f55d30
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-01-03 14:54:05 +0530

    add(Parser): parse

--- include/incanti.hpp
+++ include/incanti.hpp
@@ -5,16 +5,45 @@
 
 /*
  * Rules
- * i. Class and Namespace names in Pascal Case
- * ii. others in snake case.
+ * i. Class and Namespace names in PascalCase
+ * ii. others in snake_case.
  */
 
 namespace Incanti {
 class Parser {
 public:
-  void print_help() { std::cout << "Hello, I am Incanti" << std::endl; }
+  Parser(const std::string_view &program_name = "",
+         const std::string_view &program_desc = "")
+      : program_name_(program_name), program_desc_(program_desc) {}
+
+  void parse(int argc, char *argv[]) {
+    if (argc > 0 && program_name_.empty()) {
+      program_name_ = argv[0];
+    }
+
+    for (int i{0}; i < argc; ++i) {
+      std::string arg = argv[i];
+
+      if (arg == "-h" || arg == "--help") {
+        print_help();
+        exit(0);
+      }
+    }
+  }
+
+  void print_help() {
+    if (!program_name_.empty()) {
+      std::cout << "Usage: " << program_name_ << " [options]" << std::endl;
+    }
+
+    if (!program_desc_.empty()) {
+      std::cout << program_desc_ << std::endl;
+    }
+  }
 
 private:
+  std::string_view program_name_;
+  std::string_view program_desc_;
 };
 } // namespace Incanti
 
--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,7 @@
 #include "incanti.hpp"
 
 int main(int argc, char *argv[]) {
-  Incanti::Parser p;
-  p.print_help();
+  Incanti::Parser p("incanti-test", "program to test incanti");
+  p.parse(argc, argv);
   return 0;
 }
